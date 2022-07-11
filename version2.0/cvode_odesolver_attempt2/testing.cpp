758a57508aca59e51b9c1ed7ecfd2b431538b9ea
YoctoMac <yoctoyotta1024@yahoo.com>
2022-07-11 11:58:09 +0200

    used cmake to generate makefile for running main() in testing.cpp with cvode libraries and including my .hpp dependencies

--- version2.0/cvode_odesolver_attempt2/testing.cpp
+++ version2.0/cvode_odesolver_attempt2/testing.cpp
@@ -4,292 +4,295 @@
 
 #include "../claras_SDinit.hpp"
 #include "../claras_SDconstants.hpp"
+#include "cvode_odesolver.hpp"
+
 
 namespace dlc = dimless_constants;
 
 
-// --------------------------------- cvode header ------------------------- //
-//#include <iostream>
 
-#include <cvode/cvode.h>               /* prototypes for CVODE fcts., consts.  */
-#include <nvector/nvector_serial.h>    /* access to serial N_Vector            */
-#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
-#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
+// // --------------------------------- cvode header ------------------------- //
+// //#include <iostream>
 
-#include "differentials.hpp"
-//#include "cvode_differentials.hpp"
+// #include <cvode/cvode.h>               /* prototypes for CVODE fcts., consts.  */
+// #include <nvector/nvector_serial.h>    /* access to serial N_Vector            */
+// #include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
+// #include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
 
-using namespace std;
+// //#include "differentials.hpp"
+// #include "cvode_differentials.hpp"
 
-
-class CvodeOdeSolver
-/* CVODE ODE Solver class */
-{
-  private:
-
-  public:
-    /* SUNDIALS solver stuff */
-    SUNContext sunctx;
-    SUNMatrix A;
-    SUNLinearSolver LS;
-    void *cvode_mem;
-    int retval;                                           // reusable return flag
-    
-    /* ODE problem stuff */
-    static const int NEQ = 4;
-    UserData data;
-    realtype t;
-    N_Vector y;
-    N_Vector ATOLS; 
-    realtype RTOL;
-
-    /* constructor functions */
-    CvodeOdeSolver();
-        
-    int setup_ODE_solver(const double i_rtol, const double i_atols[NEQ],
-                      const double y_init[NEQ], const double t0);
-
-    void init_userdata(const double w, const bool doThermo);
-
-    int advance_solution(const double tout);
-
-    int reinitialise(const double tout, const double re_y[NEQ]);
-
-    int check_retval(void *returnvalue, const char *funcname, int opt);
-    
-    int print_init_ODEdata(const int nout, const double t0,
-        const double t1, const double tstep);
-
-    void destroy_cvode(); 
-};
+// using namespace std;
 
 
-
-// -------------------------------------------------------------------- //
-
-
-// --------------------------------- cvode cpp ------------------------- //
-
-
-
-
-CvodeOdeSolver::CvodeOdeSolver()
-{
-  /* initialise vectors, matrix and solver */
-  data = (UserData) malloc(sizeof *data);
-  y = NULL;
-  ATOLS = NULL;
-  A = NULL;
-  LS = NULL;
-  cvode_mem = NULL;
-}
-
-
-
-
-
-void CvodeOdeSolver::init_userdata(const double w, const bool doThermo)
-{
-  data->w = w;
-  data->doThermo = doThermo;
-};
-
-
-
-
-int CvodeOdeSolver::setup_ODE_solver(const double i_rtol, const double i_atols[NEQ],
-                      const double y_init[NEQ], const double t0)
-{
- 
-  /* 0. Create the SUNDIALS context */
-  retval = SUNContext_Create(NULL, &sunctx);
-  if (check_retval(&retval, "SUNContext_Create", 1)){ retval = 1;}
-
-  /*  1. Initialize parallel or multi-threaded environment, if appropriate. */
-  // ---------------------------------------------------------------------- //
-  
-  /* 2. Set the scalar relative and vector absolute tolerances */
-  RTOL = i_rtol;
-  ATOLS = N_VNew_Serial(NEQ, sunctx);
-  if (check_retval((void *)ATOLS, "N_VNew_Serial", 0)) return(1);
-
-  for(int i=0; i<NEQ; i++)
-  {
-    NV_Ith_S(ATOLS,i) = i_atols[i];
-  }
-
-  /* 3. initialise y vector with initial conditions */
-  y = N_VNew_Serial(NEQ, sunctx);
-  if (check_retval((void *)y, "N_VNew_Serial", 0)) return(1);
-  for(int i=0; i<NEQ; i++)
-  {
-    NV_Ith_S(y,i) = y_init[i];
-  }
- 
-  /* 4. Call CVodeCreate to create the solver memory and specify the
-   * Backward Differentiation Formula (CV_BDF) */
-  cvode_mem = CVodeCreate(CV_BDF, sunctx);
-  if (check_retval((void *)cvode_mem, "CVodeCreate", 0)) return(1);
-
-  /* 5. Call CVodeInit to initialize the integrator memory and specify the
-   * user's right hand side function in y'=f(t,y), the initial time T0, and
-   * the initial dependent variable vector y. */
-  retval = CVodeInit(cvode_mem, odes_func, t0, y);
-  if (check_retval(&retval, "CVodeInit", 1)) return(1);
-  
-  /* 6. Set linear solver optional inputs. 
-   * Provide user data which can be accessed in user provided routines */
-  retval = CVodeSetUserData(cvode_mem, data);
-  if (check_retval((void *)&retval, "CVodeSetUserData", 1)) return(1);
-  
-  /* 7. Call CVodeSVtolerances to specify the scalar relative tolerance
-   * and vector absolute tolerances */
-  retval = CVodeSVtolerances(cvode_mem, RTOL, ATOLS);
-  if (check_retval(&retval, "CVodeSVtolerances", 1)) return(1);
-
-  /* 8. Create dense SUNMatrix for use in linear solves */
-  A = SUNDenseMatrix(NEQ, NEQ, sunctx);
-  if (check_retval((void *)A, "SUNDenseMatrix", 0)) return(1);
-
-  /* 9. Create dense SUNLinearSolver object for use by CVode */
-  LS = SUNLinSol_Dense(y, A, sunctx);
-  if (check_retval((void *)LS, "SUNLinSol_Dense", 0)) return(1);
-  
-  /* 10. Attach the matrix and linear solver to CVODE */
-  retval = CVodeSetLinearSolver(cvode_mem, LS, A);
-  if (check_retval(&retval, "CVodeSetLinearSolver", 1)) return(1);
- 
-  return 0;
-};
-
-
-
-
-int CvodeOdeSolver::advance_solution(const double tout)
-/* Advance ODE solution in time */
-{
-
-  //cout << " -- t of ODE SOLVER: " << t << endl;
-  retval = CVode(cvode_mem, tout, y, &t, CV_NORMAL);
-  if (check_retval(&retval, "CVode", 1)) return 1;
-  //cout << "t of ODE SOLVER -> " << tout << endl;
-
-  return 0;
-}
-
-
-
-// int CvodeOdeSolver::reinitialise(const double tout, const double re_y[NEQ])
-// /* Reinitialize the solver after discontinuous change in y */
+// class CvodeOdeSolver
+// /* CVODE ODE Solver class */
 // {
-//   retval = CVodeReInit(cvode_mem, tout, re_y);
-//   if (check_retval((void *)&retval, "CVodeReInit", 1)) return(1);
+//   private:
+
+//   public:
+//     /* SUNDIALS solver stuff */
+//     SUNContext sunctx;
+//     SUNMatrix A;
+//     SUNLinearSolver LS;
+//     void *cvode_mem;
+//     int retval;                                           // reusable return flag
+    
+//     /* ODE problem stuff */
+//     static const int NEQ = 4;
+//     UserData data;
+//     realtype t;
+//     N_Vector y;
+//     N_Vector ATOLS; 
+//     realtype RTOL;
+
+//     /* constructor functions */
+//     CvodeOdeSolver();
+        
+//     int setup_ODE_solver(const double i_rtol, const double i_atols[NEQ],
+//                       const double y_init[NEQ], const double t0);
+
+//     void init_userdata(const double w, const bool doThermo);
+
+//     int advance_solution(const double tout);
+
+//     int reinitialise(const double tout, const double re_y[NEQ]);
+
+//     int check_retval(void *returnvalue, const char *funcname, int opt);
+    
+//     int print_init_ODEdata(const int nout, const double t0,
+//         const double t1, const double tstep);
+
+//     void destroy_cvode(); 
+// };
+
+
+
+// // -------------------------------------------------------------------- //
+
+
+// // --------------------------------- cvode cpp ------------------------- //
+
+
+
+
+// CvodeOdeSolver::CvodeOdeSolver()
+// {
+//   /* initialise vectors, matrix and solver */
+//   data = (UserData) malloc(sizeof *data);
+//   y = NULL;
+//   ATOLS = NULL;
+//   A = NULL;
+//   LS = NULL;
+//   cvode_mem = NULL;
+// }
+
+
+
+
+
+// void CvodeOdeSolver::init_userdata(const double w, const bool doThermo)
+// {
+//   data->w = w;
+//   data->doThermo = doThermo;
+// };
+
+
+
+
+// int CvodeOdeSolver::setup_ODE_solver(const double i_rtol, const double i_atols[NEQ],
+//                       const double y_init[NEQ], const double t0)
+// {
+ 
+//   /* 0. Create the SUNDIALS context */
+//   retval = SUNContext_Create(NULL, &sunctx);
+//   if (check_retval(&retval, "SUNContext_Create", 1)){ retval = 1;}
+
+//   /*  1. Initialize parallel or multi-threaded environment, if appropriate. */
+//   // ---------------------------------------------------------------------- //
+  
+//   /* 2. Set the scalar relative and vector absolute tolerances */
+//   RTOL = i_rtol;
+//   ATOLS = N_VNew_Serial(NEQ, sunctx);
+//   if (check_retval((void *)ATOLS, "N_VNew_Serial", 0)) return(1);
+
+//   for(int i=0; i<NEQ; i++)
+//   {
+//     NV_Ith_S(ATOLS,i) = i_atols[i];
+//   }
+
+//   /* 3. initialise y vector with initial conditions */
+//   y = N_VNew_Serial(NEQ, sunctx);
+//   if (check_retval((void *)y, "N_VNew_Serial", 0)) return(1);
+//   for(int i=0; i<NEQ; i++)
+//   {
+//     NV_Ith_S(y,i) = y_init[i];
+//   }
+ 
+//   /* 4. Call CVodeCreate to create the solver memory and specify the
+//    * Backward Differentiation Formula (CV_BDF) */
+//   cvode_mem = CVodeCreate(CV_BDF, sunctx);
+//   if (check_retval((void *)cvode_mem, "CVodeCreate", 0)) return(1);
+
+//   /* 5. Call CVodeInit to initialize the integrator memory and specify the
+//    * user's right hand side function in y'=f(t,y), the initial time T0, and
+//    * the initial dependent variable vector y. */
+//   retval = CVodeInit(cvode_mem, odes_func, t0, y);
+//   if (check_retval(&retval, "CVodeInit", 1)) return(1);
+  
+//   /* 6. Set linear solver optional inputs. 
+//    * Provide user data which can be accessed in user provided routines */
+//   retval = CVodeSetUserData(cvode_mem, data);
+//   if (check_retval((void *)&retval, "CVodeSetUserData", 1)) return(1);
+  
+//   /* 7. Call CVodeSVtolerances to specify the scalar relative tolerance
+//    * and vector absolute tolerances */
+//   retval = CVodeSVtolerances(cvode_mem, RTOL, ATOLS);
+//   if (check_retval(&retval, "CVodeSVtolerances", 1)) return(1);
+
+//   /* 8. Create dense SUNMatrix for use in linear solves */
+//   A = SUNDenseMatrix(NEQ, NEQ, sunctx);
+//   if (check_retval((void *)A, "SUNDenseMatrix", 0)) return(1);
+
+//   /* 9. Create dense SUNLinearSolver object for use by CVode */
+//   LS = SUNLinSol_Dense(y, A, sunctx);
+//   if (check_retval((void *)LS, "SUNLinSol_Dense", 0)) return(1);
+  
+//   /* 10. Attach the matrix and linear solver to CVODE */
+//   retval = CVodeSetLinearSolver(cvode_mem, LS, A);
+//   if (check_retval(&retval, "CVodeSetLinearSolver", 1)) return(1);
+ 
+//   return 0;
+// };
+
+
+
+
+// int CvodeOdeSolver::advance_solution(const double tout)
+// /* Advance ODE solution in time */
+// {
+
+//   //cout << " -- t of ODE SOLVER: " << t << endl;
+//   retval = CVode(cvode_mem, tout, y, &t, CV_NORMAL);
+//   if (check_retval(&retval, "CVode", 1)) return 1;
+//   //cout << "t of ODE SOLVER -> " << tout << endl;
 
 //   return 0;
 // }
 
 
 
+// // int CvodeOdeSolver::reinitialise(const double tout, const double re_y[NEQ])
+// // /* Reinitialize the solver after discontinuous change in y */
+// // {
+// //   retval = CVodeReInit(cvode_mem, tout, re_y);
+// //   if (check_retval((void *)&retval, "CVodeReInit", 1)) return(1);
 
-/*
- * Check function return value...
- *   opt == 0 means SUNDIALS function allocates memory so check if
- *            returned NULL pointer
- *   opt == 1 means SUNDIALS function returns an integer value so check if
- *            retval < 0
- *   opt == 2 means function allocates memory so check if returned
- *            NULL pointer
- */
-int CvodeOdeSolver::check_retval(void *returnvalue, const char *funcname, int opt)
-{
-  int *retval;
-
-  /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
-  if (opt == 0 && returnvalue == NULL) {
-    cout << stderr << "\nCVODE_SUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n"
-            << funcname << " " << endl;
-    return(1); }
-
-  /* Check if retval < 0 */
-  else if (opt == 1) {
-    retval = (int *) returnvalue;
-    if (*retval < 0) {
-      cout << stderr << "\nCVODE_SUNDIALS_ERROR: %s() failed with retval = %d\n\n"
-             << funcname << " " << *retval << endl;
-      return(1); }}
-
-  /* Check if function returned NULL pointer - no memory allocated */
-  else if (opt == 2 && returnvalue == NULL) {
-    cout << stderr << "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n"
-            << funcname << endl;
-    return(1); }
-
-  return(0);
-}
-
-
-
-int CvodeOdeSolver::print_init_ODEdata(const int nout, const double t0,
-        const double t1, const double tstep)
-{
-
-  if (y == NULL)
-  { 
-    retval = -1;
-    if(check_retval(&retval, "print_init_ODEdata", 1)) return(1);
-    return retval;
-  }
-
-  cout << "---------------------------------------------" << endl;
-  cout << "No. Equations (NEQ) = " << NEQ << endl;
-  cout << "y0      = " << NV_Ith_S(y,0) << endl;
-  cout << "y1      = " << NV_Ith_S(y, 1) << endl;
-  cout << "y2      = " << NV_Ith_S(y, 2) << endl;
-  cout << "y3      = " << NV_Ith_S(y, 3) << endl;
-  cout << "---------------------------------------------" << endl;
-  cout << "RTOL        = " << RTOL << endl;
-  cout << "ATOLS[0]    = " << NV_Ith_S(ATOLS, 0) << endl;
-  cout << "ATOLS[1]    = " << NV_Ith_S(ATOLS, 1) << endl;
-  cout << "ATOLS[2]    = " << NV_Ith_S(ATOLS, 2) << endl;
-  cout << "ATOLS[3]    = " << NV_Ith_S(ATOLS, 3) << endl;
-  cout << "---------------------------------------------" << endl;
-  cout << "inital t    = " << t0 << endl;
-  cout << "first t     = " << t1 << endl;
-  cout << "t step      = " << tstep << endl;
-  cout << "no. outputs = " << nout << endl;
-  cout << "---------------------------------------------\n" << endl;
-
-
-  retval = 0;
-  if(check_retval(&retval, "print_init_ODEdata", 1)) return(1);
-  return retval;
-};
+// //   return 0;
+// // }
 
 
 
 
-void CvodeOdeSolver::destroy_cvode()
-{
+// /*
+//  * Check function return value...
+//  *   opt == 0 means SUNDIALS function allocates memory so check if
+//  *            returned NULL pointer
+//  *   opt == 1 means SUNDIALS function returns an integer value so check if
+//  *            retval < 0
+//  *   opt == 2 means function allocates memory so check if returned
+//  *            NULL pointer
+//  */
+// int CvodeOdeSolver::check_retval(void *returnvalue, const char *funcname, int opt)
+// {
+//   int *retval;
 
-  /* print final statistics to the terminal screen */
-  printf("\nLast Iteration Statistics:\n");
-  retval = CVodePrintAllStats(cvode_mem, stdout, SUN_OUTPUTFORMAT_TABLE);
+//   /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
+//   if (opt == 0 && returnvalue == NULL) {
+//     cout << stderr << "\nCVODE_SUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n"
+//             << funcname << " " << endl;
+//     return(1); }
 
-  /* free memory */
-  N_VDestroy(y);                            /* Free y vector */
-  N_VDestroy(ATOLS);                       /* Free abstol vector */
-  free(data);                               /* free user_data pointer struc */
-  CVodeFree(&cvode_mem);                    /* Free CVODE memory */
-  SUNLinSolFree(LS);                        /* Free the linear solver memory */
-  SUNMatDestroy(A);                         /* Free the matrix memory */
-  SUNContext_Free(&sunctx);                 /* Free the SUNDIALS context */
+//   /* Check if retval < 0 */
+//   else if (opt == 1) {
+//     retval = (int *) returnvalue;
+//     if (*retval < 0) {
+//       cout << stderr << "\nCVODE_SUNDIALS_ERROR: %s() failed with retval = %d\n\n"
+//              << funcname << " " << *retval << endl;
+//       return(1); }}
 
-}
+//   /* Check if function returned NULL pointer - no memory allocated */
+//   else if (opt == 2 && returnvalue == NULL) {
+//     cout << stderr << "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n"
+//             << funcname << endl;
+//     return(1); }
+
+//   return(0);
+// }
+
+
+
+// int CvodeOdeSolver::print_init_ODEdata(const int nout, const double t0,
+//         const double t1, const double tstep)
+// {
+
+//   if (y == NULL)
+//   { 
+//     retval = -1;
+//     if(check_retval(&retval, "print_init_ODEdata", 1)) return(1);
+//     return retval;
+//   }
+
+//   cout << "---------------------------------------------" << endl;
+//   cout << "No. Equations (NEQ) = " << NEQ << endl;
+//   cout << "y0      = " << NV_Ith_S(y,0) << endl;
+//   cout << "y1      = " << NV_Ith_S(y, 1) << endl;
+//   cout << "y2      = " << NV_Ith_S(y, 2) << endl;
+//   cout << "y3      = " << NV_Ith_S(y, 3) << endl;
+//   cout << "---------------------------------------------" << endl;
+//   cout << "RTOL        = " << RTOL << endl;
+//   cout << "ATOLS[0]    = " << NV_Ith_S(ATOLS, 0) << endl;
+//   cout << "ATOLS[1]    = " << NV_Ith_S(ATOLS, 1) << endl;
+//   cout << "ATOLS[2]    = " << NV_Ith_S(ATOLS, 2) << endl;
+//   cout << "ATOLS[3]    = " << NV_Ith_S(ATOLS, 3) << endl;
+//   cout << "---------------------------------------------" << endl;
+//   cout << "inital t    = " << t0 << endl;
+//   cout << "first t     = " << t1 << endl;
+//   cout << "t step      = " << tstep << endl;
+//   cout << "no. outputs = " << nout << endl;
+//   cout << "---------------------------------------------\n" << endl;
+
+
+//   retval = 0;
+//   if(check_retval(&retval, "print_init_ODEdata", 1)) return(1);
+//   return retval;
+// };
 
 
 
 
-// ---------------------------------------------------------------- //
+// void CvodeOdeSolver::destroy_cvode()
+// {
+
+//   /* print final statistics to the terminal screen */
+//   printf("\nLast Iteration Statistics:\n");
+//   retval = CVodePrintAllStats(cvode_mem, stdout, SUN_OUTPUTFORMAT_TABLE);
+
+//   /* free memory */
+//   N_VDestroy(y);                            /* Free y vector */
+//   N_VDestroy(ATOLS);                       /* Free abstol vector */
+//   free(data);                               /* free user_data pointer struc */
+//   CVodeFree(&cvode_mem);                    /* Free CVODE memory */
+//   SUNLinSolFree(LS);                        /* Free the linear solver memory */
+//   SUNMatDestroy(A);                         /* Free the matrix memory */
+//   SUNContext_Free(&sunctx);                 /* Free the SUNDIALS context */
+
+// }
+
+
+
+
+// // ---------------------------------------------------------------- //
 
 
 
